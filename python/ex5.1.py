2b055f6efb6e9ba674dd49956900eb621ba9f3b5
michalfoc <33692374+michalfoc@users.noreply.github.com>
2017-11-15 15:27:52 +0000

    added colon at the end (typo)

--- python/ex5.1.py
+++ python/ex5.1.py
@@ -9,7 +9,7 @@ x = input("Is that inches or centymeters?('in' for inches, 'cm' for centymeters)
 weight = float(input("How mcuh do you weigh?\n"))
 y = input("Is that in punds or kilograms?('lbs' for pound, 'kg' for kilograms)\n")
 
-if x == 'in' and y == 'lbs'
+if x == 'in' and y == 'lbs':
     height = int(height * 2.54)
     weight = int(weight / 2.2)
     print(f"So, Your name is {name}, You are {age} years old, You are {height} cm tall and {weight} kilograms heavy.")
