1049cac528cebf23939ae31ce862d957ca76cf9c
michalfoc <33692374+michalfoc@users.noreply.github.com>
2017-11-16 09:59:35 +0000

    new file

---
+++ python/ex13.1.1.py
@@ -0,0 +1,6 @@
+# ex13.1
+
+from sys import argv
+script, name, last_name, age, height = argv
+
+print(f"This file is {script}. It was written by {name} {last_name}, who is {age} years old and {height} tall.")
